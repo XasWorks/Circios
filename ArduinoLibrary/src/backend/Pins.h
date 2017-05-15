71c4c6677a613b376e82e2ac01c5932a0d5a0f76
David Bailey <davidbailey.2889@gmail.com>
2017-05-15 13:27:59 +0200

    Pins.h für eine zentrale, definierende Datei.

---
+++ ArduinoLibrary/src/backend/Pins.h
@@ -0,0 +1,20 @@
+
+#ifndef PINS_H
+#define PINS_H
+
+// Motor input register: PORTD (Due to INT0/INT1)
+#define MOTOR_A_C1	PD2
+#define MOTOR_A_C2	PD4
+#define MOTOR_B_C1	PD3
+#define MOTOR_B_C2	PD7
+
+// Motor control register: PORTB (Mainly due to OC1A and OC1B being there)
+#define MOTOR_A_DIR	PB0
+#define MOTOR_A_PWM	PB1
+#define MOTOR_B_DIR	PB4
+#define MOTOR_B_PWM	PB2
+
+#define TICKS_PER_REV	412.5
+#define TICKS_PER_TOOTH	TICKS_PER_REV/13
+
+#endif
