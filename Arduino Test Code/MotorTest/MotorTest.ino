98fb1f762503ceb14899929a5fa3bcf1769d7010
David Bailey <davidbailey.2889@gmail.com>
2017-07-01 16:47:16 +0200

    Speed-Changes for testing

--- Arduino Test Code/MotorTest/MotorTest.ino
+++ Arduino Test Code/MotorTest/MotorTest.ino
@@ -2,16 +2,31 @@
 
 void setup() {
   Serial.begin(9600);
-  Hamster::begin(1);
+  Hamster::begin(0.75);
 
   pinMode(13, OUTPUT);
 }
 
 uint8_t mPresc = 100;
 void loop() {
-  Hamster::linksUm();
-  Hamster::linksUm();
-  delay(500);
+  delay(3000);
   Hamster::vor();
-  delay(500);
+  Hamster::linksUm();
+  Hamster::vor();
+
+  delay(2000);
+
+  Hamster::linksUm();
+  Hamster::linksUm();
+  Hamster::vor();
+
+  for(uint8_t i=0; i<3; i++)
+    Hamster::linksUm();
+
+  Hamster::vor();
+
+  Hamster::linksUm();
+  Hamster::linksUm();
+
+  delay(2000);
 }
